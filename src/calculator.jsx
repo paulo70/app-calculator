cdd14c7a7b0ced2a37aa24e49e0f6055da0a3832
paulo70 <paulofrontend@gmail.com>
2020-06-17 11:28:12 -0300

    create hooks to initial state and get of button value

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap'
 import './calculator.css';
 
 function Calculator() {
+
+  const [ displayNumbers, setDisplayNumbers ] = useState('0')
+
+  function addNumber(number){
+    setDisplayNumbers(displayNumbers + number)
+  }
+
+  function setOperation(op) {
+    setDisplayNumbers(op)
+  }
+
   return (
     <Jumbotron style={{
       background: 'transparent !important',
@@ -20,61 +31,62 @@ function Calculator() {
         <Col xs='9'>
           <Form.Control
             type='text'
-            name='txtNumeros'
+            name='displayNumbers'
             className='text-right'
             readOnly='readonly'
+            value = {displayNumbers}
            />
         </Col>
       </Row>
 
       <Row>
         <Col>
-          <Button variant='light'> 7</Button>
+          <Button variant='light' onClick = {() => addNumber('7')}> 7</Button>
         </Col>
         <Col>
-          <Button variant='light'> 8</Button>
+          <Button variant='light' onClick = {() => addNumber('8')}> 8</Button>
         </Col>
         <Col>
-          <Button variant='light'> 9</Button>
+          <Button variant='light' onClick = {() => addNumber('9')}> 9</Button>
         </Col>
         <Col>
-          <Button variant='warning'> / </Button>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col>
-          <Button variant='light'> 4</Button>
-        </Col>
-        <Col>
-          <Button variant='light'> 5</Button>
-        </Col>
-        <Col>
-          <Button variant='light'> 6</Button>
-        </Col>
-        <Col>
-          <Button variant='warning'> * </Button>
+          <Button variant='warning' onClick = {() => setOperation('/')}> / </Button>
         </Col>
       </Row>
 
       <Row>
         <Col>
-          <Button variant='light'> 1</Button>
+          <Button variant='light' onClick = {() => addNumber('4')}> 4</Button>
         </Col>
         <Col>
-          <Button variant='light'> 2</Button>
+          <Button variant='light' onClick = {() => addNumber('5')}> 5</Button>
         </Col>
         <Col>
-          <Button variant='light'> 3</Button>
+          <Button variant='light' onClick = {() => addNumber('6')}> 6</Button>
         </Col>
         <Col>
-          <Button variant='warning'> - </Button>
+          <Button variant='warning' onClick = {() => setOperation('*')}> * </Button>
         </Col>
       </Row>
 
       <Row>
         <Col>
-          <Button variant='light'> 0</Button>
+          <Button variant='light' onClick = {() => addNumber('1')}> 1</Button>
+        </Col>
+        <Col>
+          <Button variant='light' onClick = {() => addNumber('2')}> 2</Button>
+        </Col>
+        <Col>
+          <Button variant='light' onClick = {() => addNumber('3')}> 3</Button>
+        </Col>
+        <Col>
+          <Button variant='warning' onClick = {() => setOperation('-')}> - </Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Button variant='light' onClick = {() => addNumber('0')}> 0</Button>
         </Col>
         <Col>
           <Button variant='light'> .</Button>
@@ -83,7 +95,7 @@ function Calculator() {
           <Button variant='success'> = </Button>
         </Col>
         <Col>
-          <Button variant='warning'> + </Button>
+          <Button variant='warning' onClick = {() => setOperation('+')}> + </Button>
         </Col>
       </Row>
     </Container>
