350f468bdadba64df5831e8ee3c9d9c0f26ba15f
paulo70 <paulofrontend@gmail.com>
2020-06-17 11:05:34 -0300

    override css files to calculator and build layout

--- src/calculator.test.jsx
+++ src/calculator.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+
+import Calculator from './calculator';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Calculator />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
